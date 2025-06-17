c0afaee46634f26717cc3792ac854a5e8a3f59ad
何晨易 <hechenyi@wlan-172-31-46-27.th-luebeck.de>
2025-06-17 17:49:27 +0200

    first commit

---
+++ day01/read_data.py
@@ -0,0 +1,34 @@
+from torch.utils.data import Dataset
+
+from PIL import Image
+
+import os
+
+
+class MyData(Dataset):
+    def __init__(self, root_dir, label_dir):
+        self.root_dir = root_dir;
+        self.label_dir = label_dir;
+        self.path = os.path.join(self.root_dir, self.label_dir)
+        self.img_path = os.listdir(self.path)
+
+    def __getitem__(self, idx):
+        img_name = self.img_path[idx]
+        img_item_path = os.path.join(self.root_dir, self.label_dir, img_name)
+        img = Image.open(img_item_path)
+        label = self.label_dir
+        return img, label
+
+    def __len__self():
+        return len(self.img_path)
+
+
+root_dir = "/Users/hechenyi/ML-learning/dataset/raw-img"
+elefante_label_dir = "elefante"
+elefante_dataset = MyData(root_dir, elefante_label_dir)
+
+img, label = elefante_dataset[1]
+
+img.show()
+
+
